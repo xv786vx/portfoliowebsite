b3227254d47ab43d3c856818027d788bf3b869f8
firasaj <firas.aj@icloud.com>
2025-08-13 20:22:40 -0400

    added analytics to website

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,13 @@
 
 //import React from 'react'
-
+import { Analytics } from '@vercel/analytics/react'
 import SkillTree from './components/SkillTree'
 import './App.css'
 
 function App() {
   return (
     <div className="w-full h-screen">
+      <Analytics />
       <SkillTree />
     </div>
   )
